89be34179698723f8a489c0a6c97fbe8d31b3231
cshm <cshm225@gmail.com>
2022-03-17 19:56:12 +0900

    Ex2.java 練習問題追加

---
+++ study/Ex2.java
@@ -0,0 +1,13 @@
+public class Ex2{
+	public static void main(String args[]){
+		boolean result=true;
+		char name='駆';
+		double pie=3.14;
+		long num=315435661265L;
+		String msg="ミナトの攻撃";
+		System.out.println(result);
+		System.out.println(name);
+	  System.out.printf("%f \n%d\n",pie,num);
+		System.out.println(msg);
+	}
+}
