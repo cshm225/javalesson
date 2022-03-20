e7aea8db8959b1f2d2d8a07d1dd1e437972d1f1f
cshm <cshm225@gmail.com>
2022-03-21 08:08:09 +0900

    条件分岐 If1.java

---
+++ study/If1.java
@@ -0,0 +1,11 @@
+public class If1{
+	public static void main(String[] args){
+		boolean tenki=false;
+		if(tenki==true){
+			System.out.println("選択する");
+			System.out.println("散歩する");
+		}else{
+			System.out.println("eigamiru");
+		}
+	}
+}
