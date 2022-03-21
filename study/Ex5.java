5eddbee6e76106f226442aff7cbbeae1ef47149c
cshm <cshm225@gmail.com>
2022-03-21 11:00:25 +0900

    練習3-3追加 Ex5.java

---
+++ study/Ex5.java
@@ -0,0 +1,12 @@
+public class Ex5{
+	public static void main(String[] args){
+		int isHungry=0;
+		String food="syake";
+		if(isHungry==0){
+			System.out.println("お腹いっぱい");
+		}else{
+			System.out.println("腹ペコ");
+			System.out.println(food+"をいただきます");
+		}
+	}
+}
