5fb3905e24f6154246b255d8adc9492de2ec587e
cshm <cshm225@gmail.com>
2022-03-22 17:16:17 +0900

    配列基礎Array.java

--- study/Array.java
+++ study/Array.java
@@ -1,8 +1,14 @@
 public class Array{
 	public static void main(String[] args){
-		int[] score=new int[5];
+		int[] score={20,30,40,50,80};
 		int num=score.length;
+		int sum;
+		double ave;
 		System.out.println("配列の長さは"+num);
-
+		score[0]=3;
+		System.out.println("配列1"+score[0]);
+		sum=score[0]+score[1]+score[2]+score[3]+score[4];
+		ave=sum/num;
+		System.out.println("平均は"+ave);
 	}
 }
