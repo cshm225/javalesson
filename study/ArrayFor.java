4e37319d136a6dcfab00a39a22baf77d54235d23
cshm <cshm225@gmail.com>
2022-03-22 17:38:50 +0900

    配列for文ArrayFor.java

--- study/ArrayFor.java
+++ study/ArrayFor.java
@@ -1,9 +1,13 @@
 public class ArrayFor{
 	public static void main(String[] args){
 		int[] score={20,30,40,50,80};
-		int i;
+		int sum=0,i;
+		double ave;
 		for(i=0;i<score.length;i++){
 			System.out.println(score[i]);
+			sum+=score[i];
 		}
+		ave=sum/score.length;
+		System.out.println(ave);
 	}
 }
