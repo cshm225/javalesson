d63d9ef0cad149ccd865ae6bac71a97e13931f03
cshm <cshm225@gmail.com>
2022-03-23 21:19:36 +0900

    練習問題4-2 配列のforループ拡張ループEx9.java

---
+++ study/Ex9.java
@@ -0,0 +1,12 @@
+public class Ex9{
+	public static void main(String[] args){
+		int[] moneylist={121902,8302,55100};
+		int i;
+		for(i=0;i<moneylist.length;i++){
+			System.out.println(moneylist[i]);
+		}
+		for(int value:moneylist){
+			System.out.println(value);
+		}
+	}
+}
