297b1efd48b4fc0995b7cb4074cd8cf00e55008e
cshm <cshm225@gmail.com>
2022-04-01 15:28:23 +0900

    a

--- study/Yusha.java
+++ study/Yusha.java
@@ -1,7 +1,27 @@
 import java.util.*;
 public class Yusha{
-	String name;
+	String name="輝";
 	int hp;
-	String Stats;
+	public void attack(){
+	}
+	public void sleep(){
+		this.hp=100;
+		System.out.println(this.name+"は、眠って回復した");
+		System.out.printf("%sです",this.name);
+	}
+	public void sit(int sec){
+		this.hp+=sec;
+		System.out.println(this.name+"は"+sec+"秒座った");
+		System.out.println(this.name+"は"+sec+"回復した");
 
+	}
+	public void slip(){
+		this.hp-=5;
+		System.out.println(this.name+"は転んだ");
+		System.out.println("5のダメージ");
+	}
+	public void run(){
+		
+		System.out.printf(this.name+"は逃げ出した\ngameover\n最終hpは%dでした",this.hp);
+	}
 }
--- study/YushaMain.java
+++ study/YushaMain.java
@@ -1,7 +1,14 @@
 import java.util.*;
 public class YushaMain{
 	public static void main(String[] args){
-
+		Yusha h=new Yusha();	
+		h.hp=100;
+		h.name="輝";
+		System.out.printf("勇者%sを生み出しました\n",h.name);
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 }
 
